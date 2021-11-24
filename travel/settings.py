f7b1f6f317363a170784e80ce70c23fb7e045f29
Jia Yong <jiayong1008@gmail.com>
2021-11-24 11:42:54 +0800

    Fixed security issues

--- travel/settings.py
+++ travel/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '!!i8c=jb1n4*sbym0)7g-1k30!y&v*!h^6bm@7&ge+dr@h3l06'
+"""SECRET_KEY = YOUR_SECRET_KEY"""
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
